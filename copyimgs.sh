48149b5d83c182018b6a2b6748ad888376b7df86
Matthew Joyce <matthew.joyce@refeyn.com>
2024-07-26 10:52:29 +0100

    Lock QGL to the last release until AMP updates

--- copyimgs.sh
+++ copyimgs.sh
@@ -3,3 +3,4 @@ cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-image-refeyn-imx8qm-ec-asm-00014
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-image-refeyn-lite-imx8qm-ec-asm-00014-01.wic.* /home/refeyn
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-boot-imx8qm-ec-asm-00014-01-sd.bin-flash_spl /home/refeyn
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx8qm-ec-asm-00014-01-with-ec-asm-{00025-02,00027-01}.dtb /home/refeyn
+echo "Copied images to /home/refeyn"
