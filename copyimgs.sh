cab62f91859125a890d1de511a0d4a32073a90ce
Matthew Joyce <matthew.joyce@refeyn.com>
2024-08-14 16:21:12 +0100

    Add fat image and some more deps

--- copyimgs.sh
+++ copyimgs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-image-refeyn-imx8qm-ec-asm-00014-01.wic.* /home/refeyn
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-image-refeyn-lite-imx8qm-ec-asm-00014-01.wic.* /home/refeyn
+cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-image-refeyn-fat-imx8qm-ec-asm-00014-01.wic.* /home/refeyn
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx-boot-imx8qm-ec-asm-00014-01-sd.bin-flash_spl /home/refeyn
 cp tmp/deploy/images/imx8qm-ec-asm-00014-01/imx8qm-ec-asm-00014-01-with-ec-asm-{00025-02,00027-01}.dtb /home/refeyn
 echo "Copied images to /home/refeyn"
