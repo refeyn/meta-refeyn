9297a0c94ee5cee729b3aac917e5e84b12552246
Matthew Joyce <matthew.joyce@refeyn.com>
2024-11-19 12:11:14 +0000

    Fixup workflow; and make AMP work

--- copyimgs.sh
+++ copyimgs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm /home/refeyn/Apalis-iMX8-refeyn_Refeyn-Lite-Image-Tezi.tar
 cp tmp/deploy/images/apalis-imx8-refeyn/Apalis-iMX8-refeyn_Refeyn-Lite-Image-Tezi.tar /home/refeyn
+rm /home/refeyn/Apalis-iMX8-refeyn_Refeyn-Image-Tezi.tar
+cp tmp/deploy/images/apalis-imx8-refeyn/Apalis-iMX8-refeyn_Refeyn-Image-Tezi.tar /home/refeyn
 echo "Copied images to /home/refeyn"
