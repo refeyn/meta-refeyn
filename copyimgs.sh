93149a1bcb6ef20bb2177736352a756bee6375d9
Matthew Joyce <matthew.joyce@refeyn.com>
2025-08-05 10:41:30 +0100

    Start building AM69 images; fixes for mouse rotation

--- copyimgs.sh
+++ copyimgs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cp -u tmp/deploy/images/apalis-imx8-refeyn/Apalis-iMX8-refeyn_Refeyn-Lite-Image-Tezi.tar /home/refeyn
-cp -u tmp/deploy/images/apalis-imx8-refeyn/Apalis-iMX8-refeyn_Refeyn-Image-Tezi.tar /home/refeyn
-cp -u tmp/deploy/images/apalis-imx8-refeyn/Apalis-iMX8-refeyn_Refeyn-Fat-Image-Tezi.tar /home/refeyn
 cp -u deploy-ti/images/aquila-am69-refeyn/Aquila-AM69-refeyn_Refeyn-Lite-Image-Tezi.tar /home/refeyn
+cp -u deploy-ti/images/aquila-am69-refeyn/Aquila-AM69-refeyn_Refeyn-Image-Tezi.tar /home/refeyn
+cp -u deploy-ti/images/aquila-am69-refeyn/Aquila-AM69-refeyn_Refeyn-Fat-Image-Tezi.tar /home/refeyn
 echo "Copied images to /home/refeyn"
--- recipes-dependencies/qt/qtbase_%.bbappend
+++ recipes-dependencies/qt/qtbase_%.bbappend
@@ -3,3 +3,6 @@ PACKAGECONFIG:class-native:append = "accessibility gles2 eglfs"
 PACKAGECONFIG:class-native:remove = "no-opengl"
 
 EXTRA_OECMAKE:append:class-target = "-DQT_QPA_DEFAULT_PLATFORM=minimal"
+
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+SRC_URI += "file://0001-Add-environment-variable-for-rotating-libinput-mouse.patch"
