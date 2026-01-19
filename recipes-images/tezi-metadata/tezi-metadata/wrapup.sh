4516d7b9b29bceffb6991d0b70a4a917c0057b53
Matthew Joyce <matthew.joyce@refeyn.com>
2026-01-19 17:03:09 +0000

    Set up partition labels correctly

--- recipes-images/tezi-metadata/tezi-metadata/wrapup.sh
+++ recipes-images/tezi-metadata/tezi-metadata/wrapup.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
+# Mount rootfs
+cd /tmp
+mkdir m
+mount /dev/mmcblk0p2 m
+
+# Source version information
+set -o allexport
+source m/etc/os-release
+set +o allexport
+
+# Prepare chroot
+cd m
+mount --rbind /dev dev/
+
+# Relabel rootfs
+chroot . /sbin/sgdisk -c "2:rootfs_$IMAGE_VERSION" /dev/mmcblk0
+
+# Mount bootfs
+cd /tmp
+mkdir b
+mount /dev/mmcblk0p1 b
+cd b
+mv fitImage "fitImage_$IMAGE_VERSION"
+
 exit 0
