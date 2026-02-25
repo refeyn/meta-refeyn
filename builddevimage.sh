3eae5fec0b3a216cfb6dba33eb37ebc28c5335ed
Matthew Joyce <matthew.joyce@refeyn.com>
2026-02-25 15:20:55 +0000

    Remove bluetooth, wifi and 3g packages, fix dev-utils inclusion

--- builddevimage.sh
+++ builddevimage.sh
@@ -3,11 +3,11 @@ set -e
 set -o allexport
 source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/2026.3.0)
 set +o allexport
-export REFEYN_DEV_UTILS_BRANCH=release/apps_2026.3.0
-export REFEYN_REFEYN_IO_BRANCH=release/apps_2026.3.0
-export REFEYN_ISCAT_BRANCH=release/apps_2026.3.0
-export REFEYN_ISCAT_UTILS_BRANCH=release/apps_2026.3.0
-export REFEYN_LW8_BRANCH=release/2026.3.0
+export REFEYN_DEV_UTILS_BRANCH=release/apps_26.1.0
+export REFEYN_REFEYN_IO_BRANCH=release/apps_26.1.0
+export REFEYN_ISCAT_BRANCH=release/apps_26.1.0
+export REFEYN_ISCAT_UTILS_BRANCH=release/apps_26.1.0
+export REFEYN_LW8_BRANCH=release/26.1.0
 export REFEYN_NXP_SDK_BRANCH=master
 # export REFEYN_NO_CYTHONIZE=0
 env | grep REFEYN_
