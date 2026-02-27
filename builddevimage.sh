47112898949f700bc806c9aab859286e9f6a2914
Matthew Joyce <matthew.joyce@refeyn.com>
2026-02-27 11:22:21 +0000

    Remove need for submodule branches

--- builddevimage.sh
+++ builddevimage.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 set -o allexport
-source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/2026.3.0)
+source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/26.1.0)
 set +o allexport
-export REFEYN_DEV_UTILS_BRANCH=release/apps_26.1.0
-export REFEYN_REFEYN_IO_BRANCH=release/apps_26.1.0
-export REFEYN_ISCAT_BRANCH=release/apps_26.1.0
-export REFEYN_ISCAT_UTILS_BRANCH=release/apps_26.1.0
-export REFEYN_LW8_BRANCH=release/26.1.0
-export REFEYN_NXP_SDK_BRANCH=master
 # export REFEYN_NO_CYTHONIZE=0
 env | grep REFEYN_
-devtool build-image refeyn-image
+devtool build-image refeyn-image-fat
 ../meta-refeyn/copyimgs.sh
