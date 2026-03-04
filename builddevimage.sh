a68371a5e1ba26280c6810878888a047bf25193d
Matthew Joyce <matthew.joyce@refeyn.com>
2026-03-04 10:17:44 +0000

    Add jinja2 as lw8 dep

--- builddevimage.sh
+++ builddevimage.sh
@@ -5,5 +5,5 @@ source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/26.1.0)
 set +o allexport
 # export REFEYN_NO_CYTHONIZE=0
 env | grep REFEYN_
-devtool build-image refeyn-image-fat
+devtool build-image refeyn-image
 ../meta-refeyn/copyimgs.sh
