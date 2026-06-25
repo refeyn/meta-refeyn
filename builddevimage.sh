8fa70cd7a688384c056b9ac7c73be7e3950fb77a
Matthew Joyce <matthew.joyce@refeyn.com>
2026-06-25 12:02:12 +0100

    Update default branch target

--- builddevimage.sh
+++ builddevimage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -o allexport
-source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/26.1.0)
+source <(python ../meta-refeyn/resolve_commits.py $GH_TOKEN release/2026.7.0)
 set +o allexport
 # export REFEYN_NO_CYTHONIZE=0
 env | grep REFEYN_
